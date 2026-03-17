060fc69cd4ba5c86cfaaebe81d9124a5428159a7
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2026-03-18 01:42:35 +0900

    ツクールMZに対応（オートセーブのスロット数はカウント対象外とした）

--- my_plugins/HTN_SaveMax.js
+++ my_plugins/HTN_SaveMax.js
@@ -2,20 +2,40 @@
 //
 // SaveMax
 //
-// Copyright (c) 2016 hatonekoe
+// Copyright (c) 2016-2026 hatonekoe
 // This software is released under the MIT License.
 // http://opensource.org/licenses/mit-license.php
 //
+// 2026/03/18 ver2.0.0 ツクールMZに対応
 // 2016/09/12 ver1.0.0 first release
 // 2016/09/10 ver0.0.1 開発開始
 //
 // --------------------------------------------------------------------------
+
 /*:
- * @plugindesc セーブ最大数を変更します
- * @author ハトネコエ - http://hato-neko.x0.com
+ * @target MV MZ
+ * @plugindesc Change the maximum number of save slots
+ * @author nekonenene - https://hato-neko.x0.com
  *
  * @help
- * セーブできる最大数を変更します。デフォルトは 20 です
+ * Change the maximum number of save slots. (Default: 20)
+ * In RPG Maker MZ, it is counted without including the autosave.
+ *
+ * @param Max Quantity
+ * @desc The maximum number of save slots
+ * @default 20
+ *
+ */
+
+/*:ja
+ * @target MV MZ
+ * @plugindesc セーブ最大数を変更します
+ * @author ハトネコエ - https://hato-neko.x0.com
+ *
+ * @help
+ * セーブできる最大数を変更します。デフォルトは 20 です。
+ * ツクールMZでオートセーブをONにしている場合、
+ * オートセーブのスロットは含まずにカウントします。
  *
  * @param Max Quantity
  * @desc セーブ最大数
@@ -25,12 +45,16 @@
 
 (function() {
 
-  var pluginName = "HTN_SaveMax";
+  const pluginName = "HTN_SaveMax";
 
-  var parameters = PluginManager.parameters(pluginName);
-  var saveMax = Number(parameters["Max Quantity"]);
+  const parameters = PluginManager.parameters(pluginName);
+  const saveMax = Number(parameters["Max Quantity"]);
 
   DataManager.maxSavefiles = function() {
+    if (Utils.RPGMAKER_NAME === "MZ" && $gameSystem && $gameSystem.isAutosaveEnabled()) {
+      return saveMax + 1;
+    }
+
     return saveMax;
   };
 
