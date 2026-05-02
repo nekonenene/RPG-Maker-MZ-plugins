1ad071e30c31b3d33e568770722e40a8144d4b8a
ハトネコエ <hato.nekoe@gmail.com>
2026-05-03 03:41:14 +0900

    混乱の重ねがけで魅了になるセリフ実装

--- my_plugins/HTN_MonsterMessage/data/Enemy_0001.js
+++ my_plugins/HTN_MonsterMessage/data/Enemy_0001.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const ENEMY_ID = 1;
+let targetBeforeAttackStateIds = [];
 
 // 攻撃前のセリフ
 HTN_MonsterMessage.registerBeforeAttack(ENEMY_ID, ({ skill, subject, _targets, target, messages }) => {
   const rand = Math.random();
+  targetBeforeAttackStateIds = target ? target.states().map(state => state.id) : [];
 
   if (skill.name === '混乱の歌') {
     if (rand < 0.3) {
@@ -46,6 +48,20 @@ HTN_MonsterMessage.registerBeforeAttack(ENEMY_ID, ({ skill, subject, _targets, t
 
 // 攻撃後のセリフ
 HTN_MonsterMessage.registerAfterAttack(ENEMY_ID, ({ skill, subject, _targets, target, messages }) => {
+  if (skill.name === '混乱の歌') {
+    if (targetBeforeAttackStateIds.includes(32) && !target?.isStateAffected(33)) {
+      messages.push('あら〜？\n混乱が深くなったら誘惑されちゃいました？');
+      messages.flush();
+
+      messages.name = '';
+      messages.push(`${target.name()}の混乱は魅了に変化した！`);
+      messages.flush();
+
+      target.addState(33);
+      target.removeState(32);
+    }
+  }
+
   if (skill.name === '誘惑の歌') {
     if (target?.isStateAffected(33)) {
       messages.push('うっとりとした素敵な表情になりましたね♥\nかわいい♥');
