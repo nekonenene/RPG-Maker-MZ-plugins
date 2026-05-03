4c6c745ad62f818abf20cce1655ce6e1ce3d2e70
ハトネコエ <hato.nekoe@gmail.com>
2026-05-03 11:59:23 +0900

    Enemy_0002.js を、基本的なテンプレートがわかるように更新

--- my_plugins/HTN_MonsterMessage/data/Enemy_0002.js
+++ my_plugins/HTN_MonsterMessage/data/Enemy_0002.js
@@ -2,12 +2,33 @@
 
 const ENEMY_ID = 2;
 const S = HTN_MonsterMessage.STATE;
+const GV = HTN_MonsterMessage.GAME_VARIABLES;
+let targetBeforeAttackStateIds = [];
+
+// 遭遇時のセリフ
+HTN_MonsterMessage.registerEncountering(ENEMY_ID, ({ subject, targets, target, messages }) => {
+  messages.push('遭遇時のセリフのテスト');
+  messages.flush();
+});
 
 // 攻撃前のセリフ
-HTN_MonsterMessage.registerBeforeAttack(ENEMY_ID, ({ skill, subject, target, messages }) => {
+HTN_MonsterMessage.registerBeforeAttack(ENEMY_ID, ({ skill, subject, targets, target, messages, comboCount }) => {
+  targetBeforeAttackStateIds = target ? target.states().map(state => state.id) : [];
+
+  messages.push('攻撃前のセリフのテスト');
+  messages.flush();
+});
+
+// 攻撃後のセリフ
+HTN_MonsterMessage.registerAfterAttack(ENEMY_ID, ({ skill, subject, targets, target, messages, comboCount, addComboAttack }) => {
+  // 敵すべてに攻撃が当たった後のセリフ
+  if (targets.every(t => t.result().success)) {
+    messages.push('攻撃成功時のセリフ');
+  }
+
   // デフォルトメッセージ
   if (messages.pending.length === 0) {
-    messages.push('テスト');
+    messages.push('攻撃後のセリフのテスト');
   }
 
   messages.flush();
