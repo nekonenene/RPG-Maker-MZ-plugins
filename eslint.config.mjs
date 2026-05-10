42408c01854ca8c6ed67b3db5cb296985c0bba98
ハトネコエ <hato.nekoe@gmail.com>
2026-05-11 04:08:24 +0900

    eslint.config.mjs 内を single quotes で統一

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,22 @@
-import js from "@eslint/js";
+import js from '@eslint/js';
 
 export default [
   {
-    files: ["my_plugins/**/*.js"],
+    files: ['my_plugins/**/*.js'],
     ...js.configs.recommended,
     rules: {
       ...js.configs.recommended.rules,
-      "no-undef": "off", // RPGツクールMZで定義されているクラスやグローバル変数などを使うため無効化
-      "no-unused-vars": ["warn", {
-        varsIgnorePattern: "^_",
-        argsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
+      'no-undef': 'off', // RPGツクールMZで定義されているクラスやグローバル変数などを使うため無効化
+      'no-unused-vars': ['warn', {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
       }],
-      indent: ["error", 2, { SwitchCase: 1 }],
-      "linebreak-style": ["error", "unix"],
-      "no-trailing-spaces": "error",
-      "eol-last": ["error", "always"],
-      quotes: ["warn", "single", { avoidEscape: true }], // コアスクリプトでは double quotes なので warn に留める
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      'no-trailing-spaces': 'error',
+      'eol-last': ['error', 'always'],
+      quotes: ['warn', 'single', { avoidEscape: true }], // コアスクリプトでは double quotes なので warn に留める
     },
   },
 ];
