4f68b433d9574922d2a4532d2eea981e466851e9
ハトネコエ <hato.nekoe@gmail.com>
2026-05-11 04:22:55 +0900

    HTN_SmartCharm.js 内を single quotes に統一

--- my_plugins/HTN_SmartCharm/HTN_SmartCharm.js
+++ my_plugins/HTN_SmartCharm/HTN_SmartCharm.js
@@ -538,7 +538,7 @@
     if (action && subject._smartCharmShouldCancelAction && !subject.states().some(s => s.meta.SmartCharm)) {
       subject._smartCharmShouldCancelAction = false;
       // Actionフェーズへの移行処理だけおこない、ターゲットを空にして実質的にスキップする
-      this._phase = "action";
+      this._phase = 'action';
       this._action = action;
       this._targets = [];
       subject.cancelMotionRefresh();
@@ -548,13 +548,13 @@
     if (action && action._isSmartCharmStunned) {
       const stunMessage = action._smartCharmStunMessage;
       if (stunMessage) {
-        this._logWindow.push("addText", stunMessage.format(subject.name()).trim());
-        this._logWindow.push("wait"); // メッセージを読ませるためのウェイト
-        this._logWindow.push("clear");
+        this._logWindow.push('addText', stunMessage.format(subject.name()).trim());
+        this._logWindow.push('wait'); // メッセージを読ませるためのウェイト
+        this._logWindow.push('clear');
       }
 
       // Actionフェーズへの移行処理だけおこない、ターゲットを空にしてスキップする
-      this._phase = "action";
+      this._phase = 'action';
       this._action = action;
       this._targets = [];
       subject.cancelMotionRefresh();
